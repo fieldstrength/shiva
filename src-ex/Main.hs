cef0e03fad261dc3e696b9ec64f311831e8400b4
BlackBrane <cs.hbar@gmail.com>
2016-10-14 15:42:49 +0200

    Open browser window automatically

--- src-ex/Main.hs
+++ src-ex/Main.hs
@@ -1,8 +1,17 @@
 module Main where
 
-import Shiva.Server (runServer)
-import Shiva.Config (setup)
-import System.Environment (getArgs)
+import Shiva.Server        (runServer)
+import Shiva.Config        (setup)
+
+import System.Environment  (getArgs)
+import System.Process      (callCommand)
+import Control.Concurrent  (forkIO, threadDelay)
+
+
+launch :: IO ()
+launch = do
+  threadDelay $ 5*10^4  -- 50 ms
+  callCommand "open 'http://localhost:7777'"
 
 main :: IO ()
 main = do
@@ -10,5 +19,5 @@ main = do
   case args of
     ["setup"]  -> setup
     ["server"] -> runServer
-    []         -> runServer
+    []         -> forkIO launch *> runServer
     _          -> putStrLn "Unrecognized command."
