9e4ae9fd2e26dd5e3e2b8b126bbd9d61bb148b32
Cliff Harvey <cs.hbar@gmail.com>
2017-12-09 23:52:58 +0100

    Cleanup

--- src/Shiva/Server.hs
+++ src/Shiva/Server.hs
@@ -17,7 +17,7 @@ import Control.Monad.Reader          (runReaderT)
 import Data.Text                     (Text)
 import Lucid
 import Network.Wai.Middleware.Static
-import Shiva.Table.ArticleContent   (getRecent, insert)
+import Shiva.Table.ArticleContent    (getRecent, insert)
 import Web.Scotty
 
 
